871362323a59a7b167762dc3614c6670d098d9e7
Nguyen Anh Quan <anhquan291@gmail.com>
2018-04-14 21:41:39 +0700

    first commit

--- js/javascript.js
+++ js/javascript.js
@@ -10,4 +10,24 @@ $(document).ready(function(){
 
 		$('p,h4,h5,i,.benefit_ul ul li').fadeIn('8000000');
 
+		$('.benefit_ul1 img').mouseover(function() {
+			/* Act on the event */
+			$('.benefit_ul1 img').attr("width","110%");
+		});
+		$('.benefit_ul1 img').mouseout(function() {
+			/* Act on the event */
+			$('.benefit_ul1 img').attr("width","100%");
+		});
+
+$(window).scroll(function() {
+          $('.slideanim').each(function() {
+            var pos = $(this).offset().top;
+            var winTop = $(window).scrollTop();
+              if(pos < winTop + 700){
+                $(this).addClass('slide');
+              }
+
+          });
+        });
+
 });
